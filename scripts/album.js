fa1670b676441e054cd75a1521bf0e06d4d0242e
Mark Dellaposta <markdellaposta@Marks-MacBook-Pro.local>
2017-08-02 15:53:07 -0400

    Checkpoint 12 Coursework

--- scripts/album.js
+++ scripts/album.js
@@ -49,7 +49,7 @@ var albumDellaPosta = {
 var createSongRow = function(songNumber, songName, songLength) {
     var template =
         '<tr class="album-view-song-item">'
-      + '   <td class="song-item-number" data-song-number = "' + songNumber + '">' songNumber + '</td>'
+      + '   <td class="song-item-number" data-song-number = "' + songNumber + '">' + songNumber + '</td>'
       + '   <td class="song-item-title">' + songName + '</td>'
       + '   <td class="song-item-duration">' + songLength + '</td>'
       ;
