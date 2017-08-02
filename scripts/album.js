bcdc5a5dd8910e8afa71ac0401c87a1c52eed747
Mark Dellaposta <markdellaposta@Marks-MacBook-Pro.local>
2017-08-02 16:06:21 -0400

    Checkpoint 12 Coursework

--- scripts/album.js
+++ scripts/album.js
@@ -80,14 +80,14 @@ var setCurrentAlbum = function(album) {
 var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
 var songRows = document.getElementsByClassName('album-view-song-item');
 
-var playButtomTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 
 window.onload = function () {
     setCurrentAlbum(albumPicasso);
 
     songListContainer.addEventListener('mouseover', function(event){
         if (event.target.parentElement.className === 'album-view-song-item') {
-          event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtomTemplate;
+          event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
         }
     } );
 
